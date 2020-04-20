910e795ba4a466f2ebda97a3fa31f0aa43bb4199
Ruyman Reyes <Ruymanr@gmail.com>
2020-04-20 17:46:09 +0200

    build

---
+++ build/precache-manifest.8e78b07e1909978c4cdb5bcfda088ad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0076466ddc555350406575b80ff6ab67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a2bc1bb991a5f693715",
+    "url": "/static/css/main.733a94b3.chunk.css"
+  },
+  {
+    "revision": "43c176156bb86b09c92c",
+    "url": "/static/js/2.72a66e27.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.72a66e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2bc1bb991a5f693715",
+    "url": "/static/js/main.b07ac44e.chunk.js"
+  },
+  {
+    "revision": "a40662e75da12101abdb",
+    "url": "/static/js/runtime-main.4ac1cc8c.js"
+  }
+]);
